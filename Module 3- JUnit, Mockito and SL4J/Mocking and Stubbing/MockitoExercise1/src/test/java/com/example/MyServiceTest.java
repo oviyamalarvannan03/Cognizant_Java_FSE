62b096a4786b24754553dd9ce2f84683c94c9781
Oviya Malarvannan <oviyamalarvannan0305@gmail.com>
2026-06-26 19:33:43 +0530

    Add MockitoExercise1 with output screenshot for Mocking and Stubbing

---
+++ Module 3- JUnit, Mockito and SL4J/Mocking and Stubbing/MockitoExercise1/src/test/java/com/example/MyServiceTest.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class MyServiceTest {
+
+    @Test
+    void testExternalApi() {
+
+        // Create Mock
+        ExternalApi mockApi = mock(ExternalApi.class);
+
+        // Stub Method
+        when(mockApi.getData()).thenReturn("Mock Data");
+
+        // Inject Mock
+        MyService service = new MyService(mockApi);
+
+        // Call Method
+        String result = service.fetchData();
+
+        // Verify Result
+        assertEquals("Mock Data", result);
+    }
+}
